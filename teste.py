de4964f63b9d4acd5f93640f94977fe7c4bf9125
Matheus Custódio <55003569+Matheus-C@users.noreply.github.com>
2023-07-07 16:59:12 -0300

    algumas modificações leves e comentários

--- teste.py
+++ teste.py
@@ -1,24 +1,26 @@
 from empath import Empath
 from operator import add
 from pyspark.sql import SparkSession
-from pyspark.ml.feature import Tokenizer
-from pyspark.ml.feature import StopWordsRemover
-import pyspark.sql.functions as f
 from sys import argv
 
+#função que tokeniza a entrada
 def separa(line):
     result = []
+    #caso não seja texto não tenta executar e retorna uma lista vazia
     if isinstance(line.Lyric, str):
         result = line.Lyric.lower().split(" ")
     return result
-
+    
+#nome do arquivo .csv
 arq = argv[1]
 
+#inicio da sessão Spark
 spark = SparkSession\
   .builder \
   .appName("PythonWordCount") \
   .getOrCreate()
 
+#carrega uma lista de StopWords
 gist_file = open("gist_stopwords.txt", "r")
 try:
     content = gist_file.read()
@@ -27,19 +29,26 @@ try:
 finally:
     gist_file.close()
 
-out = open("file.txt", "w")
+#lê o arquivo csv especificado no argumento selecionando a coluna Lyric que é a qual contém as letras das músicas
 data = spark.read.format('csv').options(header='true', inferSchema='true') \
 .load(arq).select("Lyric")
-# faz o mapping de todos os rdds
+
+# faz o mapping, filtrando as stopwords e fazendo o reduce logo em seguida
 mapped_rdd = data.rdd.flatMap (lambda line: separa(line)).filter(lambda x: x not in stop_words)\
     .map(lambda word: (word, 1)).reduceByKey (add)
-word_count = mapped_rdd.takeOrdered(20, key=lambda x: -x[1])
 
-top_words = set().union((key for (key, value) in word_count))
+#captura as 20 palavras com maior ocorrência
+top20 = mapped_rdd.takeOrdered(20, key=lambda x: -x[1])
+#captura apenas as palavras (keys)
+top_words = set().union((key for (key, value) in top20))
 
+#instancia o empath e analiza as 20 palavras mais recorrentes
 lexicon = Empath()
-
 my_dict = lexicon.analyze(list(top_words), normalize=True)
+
+#abre o arquivo de saída
+out = open(arq+"_out.txt", "w")
+#para cada resultado positivo que o empath detecta é escrito a emoção detectada e sua pontuação
 for key, value in my_dict.items():
     if value > 0:
         out.write("%s: %f\n"%(key, value))
